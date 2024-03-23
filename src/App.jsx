38ebac750b72cb1e74487f5458e8ab27dca76f25
Alif Pahreza <agzmv123@gmail.com>
2024-03-23 21:12:09 +0800

    init daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-
 function App() {
 	return (
 		<>
-			<h1 className='text-2xl text-center mt-52 font-bold text-red-500'>Notes App Submission Pertama</h1>
+			<h1 className='text-2xl text-center mt-52 font-bold text-red-500'>
+				Notes App Submission Pertama
+			</h1>
+      <button className="btn btn-warning">Click here</button>
 		</>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,8 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	daisyui: {
+		themes: ['dark']
+	},
+	plugins: [require('daisyui')],
 };
