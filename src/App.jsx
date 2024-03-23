b6aa2bc3bcbdf0a7a16504e069f185a57f425389
Alif Pahreza <agzmv123@gmail.com>
2024-03-23 21:26:40 +0800

    set structure folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 function App() {
 	return (
 		<>
-			<h1 className='text-2xl text-center mt-52 font-bold text-red-500'>
-				Notes App Submission Pertama
-			</h1>
-      <button className="btn btn-warning">Click here</button>
 		</>
 	);
 }
